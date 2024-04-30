9b77d60b67fe98a71bc671f495f0668d21996a3b
EastWise1210 <fletcher_seth2335@naver.com>
2024-04-30 12:41:05 +0900

    4.29

--- HOME.py
+++ HOME.py
@@ -1,6 +1,12 @@
 import streamlit as st
 
 def run_home():
+    
+    st.image('./image/car.png')
+    st.markdown(HOME_text, unsafe_allow_html=True)
+    st.markdown(test_font2, unsafe_allow_html=True)
+
+'''
     st.markdown("<br><br>", unsafe_allow_html=True)
     st.image('./image/car.png')
     st.markdown("<h4 style='text-align:center;'>귀하의 방문을 환영합니다.</h4>", unsafe_allow_html=True)
@@ -8,9 +14,35 @@ def run_home():
     st.markdown("<p style='text-align:center;'>그리고 데이터들이 잘 가공된 정보(inforamation)를 이용하면 우리의 의사결정에 많은 도움이 됩니다.</p>", unsafe_allow_html=True)
     st.markdown("<p style='text-align:center;'>본 대시보드에서는 자동차 구매와 관련된 데이터들을 가공하여 다양한 지표들로서 사용자에게 유용한 정보들을 제공하는 것이 목적입니다.</p>", unsafe_allow_html=True)
     st.markdown("<p style='text-align:center'>좌측 메뉴에서 원하시는 서비스를 선택하실 수 있습니다.</p>", unsafe_allow_html=True)
+'''
 
 
+HOME_text ='''
+<h4 style='text-align:center;'>귀하의 방문을 환영합니다.</h4>
+<p style='text-align:center;'>우리는 현실에서 무수히 많은 데이터(data)들을 마주합니다.<br>
+이 데이터들을 정리하고 가공하여 상관관계를 파악하면 우리의 의사결정에 도움이 되는 정보(information)들을 얻을 수 있습니다.<br>
+본 대시보드에서는 자동차 구매와 관련된 데이터들을 가공하여 사용자에게 유용한 정보를 제공하는 것이 목적입니다.<br>
+좌측 메뉴에서 원하시는 서비스를 선택하실 수 있습니다.
+</p>
+'''
 
 
+test_font2 = '''
+<style>
+@font-face {
+    font-family: 'InkLipquid';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_one@1.0/InkLipquid.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
 
+	.title_font2{
+	font-family: 'InkLipquid';
+	color: white;
+	}
+</style>
+<body>
+<p class="title_font2", style='text-align:center; font-size:150px'>테스트용 텍스트</p>
+</body>
+'''
 
--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 
 import streamlit as st
+from streamlit_option_menu import option_menu
+from streamlit.components.v1 import html
+
 from HOME import run_home
 from ML import run_ml
 from EDA import run_eda
@@ -10,8 +13,8 @@ from utils import html_main_head
 #from pyparsing import empty
 
 
-st.set_page_config(layout="wide")
-#empty1,con1,empty2 = st.columns([0.3,1.0,0.3])
+#st.set_page_config(layout="wide")
+empty1,con1,empty2 = st.columns([0.2,1.5,0.2])
 #empyt1,con2,con3,empty2 = st.columns([0.3,0.5,0.5,0.3])
 #empyt1,con4,empty2 = st.columns([0.3,1.0,0.3])
 #empyt1,con5,con6,empty2 = st.columns([0.3,0.5,0.5,0.3])
@@ -26,26 +29,105 @@ st.set_page_config(layout="wide")
 
 
 
-
 def main() :
-    st.markdown(html_main_head, unsafe_allow_html=True)
 
-    menu = ['HOME', 'EDA', 'ML', 'More about']
-    user_sel1 = st.sidebar.selectbox(label='SELECT', options=menu)
+    #sidebar 블록
+    with st.sidebar:
+        option_menu_list = ['HOME', 'INTRODUCTION', 'EDA', 'MACHINE LEARNING', 'CUSTROMER SERVICE', 'ETC']
+        choose = option_menu("Welcome!", option_menu_list,
+                            icons=['house', 'camera fill', 'kanban'],
+                            menu_icon="bi bi-emoji-smile", default_index=0,
+                            styles={
+            "container": {"padding": "5!important", "background-color": "#000000"},
+            "icon": {"color": "orange", "font-size": "15px"}, 
+            "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+            "nav-link-selected": {"background-color": "#02ab21"},
+        }
+        )
 
-
-    if user_sel1 == menu[0]:
+    #메인메뉴 선택하면 해당 화면 보여주기
+    if choose == option_menu_list[0]:
         run_home()
-
-    elif user_sel1 == menu[1]:
+    elif choose == option_menu_list[1]:
+        pass
+    elif choose == option_menu_list[2]:
         run_eda()
-   
-    elif user_sel1 == menu[2]:
+    elif choose == option_menu_list[3]:
         run_ml()
-
     else :
         run_more_about()
-   
+
+
+    #column 1 블록
+    with con1:
+        st.markdown(html_main_head, unsafe_allow_html=True)
+
+        #사이드바 셀렉트박스 1
+        menu1_list = ['example1', 'example2', 'example3', 'example4', 'example5']
+        user_side_menu1 = st.sidebar.selectbox(label='SUB SELECT EX1', options=menu1_list)
+        if user_side_menu1 == menu1_list[0]:
+            pass
+        elif user_side_menu1 == menu1_list[1]:
+            pass
+    
+        elif user_side_menu1 == menu1_list[2]:
+            pass
+        elif user_side_menu1 == menu1_list[3]:
+            pass
+        else :
+            pass
+
+        #사이드바 셀렉트박스 2
+        menu2_list = ['example1', 'example2', 'example3', 'example4', 'example5']
+        user_side_menu2 = st.sidebar.selectbox(label='SUB SELECT EX2', options=menu2_list)
+        if user_side_menu2 == menu2_list[0]:
+            pass
+
+        elif user_side_menu2 == menu2_list[1]:
+            pass
+    
+        elif user_side_menu2 == menu2_list[2]:
+            pass
+        elif user_side_menu2 == menu2_list[3]:
+            pass
+        else :
+            pass
+
+
+        #사이드바 셀렉트박스 3
+        menu3_list = ['example1', 'example2', 'example3', 'example4', 'example5']
+        user_side_menu3 = st.sidebar.selectbox(label='SUB SELECT EX3', options=menu3_list)
+        if user_side_menu2 == menu2_list[0]:
+            pass
+
+        elif user_side_menu2 == menu2_list[1]:
+            pass
+    
+        elif user_side_menu2 == menu2_list[2]:
+            pass
+        elif user_side_menu2 == menu2_list[3]:
+            pass
+        else :
+            pass
+
+        #사이드바 셀렉트박스 4
+        menu4_list = ['example1', 'example2', 'example3', 'example4', 'example5']
+        user_side_menu4 = st.sidebar.selectbox(label='SUB SELECT EX4', options=menu4_list)
+        if user_side_menu2 == menu2_list[0]:
+            pass
+
+        elif user_side_menu2 == menu2_list[1]:
+            pass
+    
+        elif user_side_menu2 == menu2_list[2]:
+            pass
+        elif user_side_menu2 == menu2_list[3]:
+            pass
+        else :
+            pass
+
+
+
 
 
 if __name__ == '__main__':
